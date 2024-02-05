4305e4ed67f99d12736387249756e4e47a2fa31c
Ashwin Maurya <ashwinmaurya30@gmail.com>
2024-02-05 13:21:40 +0530

    changes

--- app/_components/Search/SearchBar.jsx
+++ app/_components/Search/SearchBar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useTransition } from "react";
 import SearchDropDown from "./SearchDropDown";
 import { searcheverything } from "@/lib/actions/search.actions";
 const SearchBar = ({ scrollDirection }) => {
@@ -7,55 +7,60 @@ const SearchBar = ({ scrollDirection }) => {
   const [searchtext, setSearchtext] = useState("");
   const [searchResult, setSearchResult] = useState({});
   const [searchResultExist, setSearchResultExist] = useState(false);
+  const [loading, setLoading] = useState(false); // New state for loading
+  const [_, startTransition] = useTransition();
 
   useEffect(() => {
     const fetchData = async () => {
-      if (searchtext.trim().length > 3) {
-        const result = await searcheverything(searchtext);
-        console.log(result);
-        setSearchResult(result);
-        setSearchResultExist(true);
+      if (searchtext.trim().length > 1) {
+        try {
+          setLoading(true); // Set loading to true when starting the search
+          const result = await searcheverything(searchtext);
+          console.log(result);
+          setSearchResult(result);
+          setSearchResultExist(true);
+        } catch (error) {
+          console.error("Error fetching search results:", error);
+        } finally {
+          setLoading(false); // Set loading to false when the search is done
+        }
       } else {
         setSearchResult({});
         setSearchResultExist(false);
       }
+      if (searchtext.trim().length !== 0) {
+        setSearchResultExist(true);
+      }
     };
 
-    fetchData();
-  }, [searchtext]);
-  // useEffect(() => {
-  //   if (results.length != 0) {
-  //     setSearchResult(results);
-  //     setSearchResultExist(true);
-  //   }
-  // }, [results]);
+    startTransition(() => {
+      fetchData();
+    });
+  }, [searchtext, startTransition]);
 
-  const handleInputChange = (event) => {
-    const { value } = event.target;
-    setSearchtext(value);
-    if (value.trim() === "") {
-      setSearchResultExist(false);
-    }
-  };
   const handleSearchIconClick = () => {
     setExpanded(!expanded);
     setSearchResultExist(false);
   };
+
   const handleInputBlur = () => {
     const blurTimeout = setTimeout(() => {
       setSearchResultExist(false);
-    }, 100);
+    }, 300);
   };
+
   const handleInputFocus = () => {
-    if (searchtext.trim() != "") {
+    if (searchtext.trim() !== "") {
       setSearchResultExist(true);
     }
   };
+
   useEffect(() => {
-    if (scrollDirection != "up") {
+    if (scrollDirection !== "up") {
       setSearchResultExist(false);
     }
   }, [scrollDirection]);
+
   return (
     <>
       {searchResultExist && <SearchDropDown searchResult={searchResult} />}
@@ -68,20 +73,100 @@ const SearchBar = ({ scrollDirection }) => {
           className="grid h-full w-14 cursor-pointer place-items-center text-gray-600"
           onClick={handleSearchIconClick}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
+          {!loading ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          ) : (
+            <svg
+              width="45"
+              height="45"
+              viewBox="0 0 45 45"
+              xmlns="http://www.w3.org/2000/svg"
+              stroke="#000"
+            >
+              <g
+                fill="none"
+                fill-rule="evenodd"
+                transform="translate(1 1)"
+                stroke-width="2"
+              >
+                <circle cx="22" cy="22" r="6" stroke-opacity="0">
+                  <animate
+                    attributeName="r"
+                    begin="1.5s"
+                    dur="3s"
+                    values="6;22"
+                    calcMode="linear"
+                    repeatCount="indefinite"
+                  />
+                  <animate
+                    attributeName="stroke-opacity"
+                    begin="1.5s"
+                    dur="3s"
+                    values="1;0"
+                    calcMode="linear"
+                    repeatCount="indefinite"
+                  />
+                  <animate
+                    attributeName="stroke-width"
+                    begin="1.5s"
+                    dur="3s"
+                    values="2;0"
+                    calcMode="linear"
+                    repeatCount="indefinite"
+                  />
+                </circle>
+                <circle cx="22" cy="22" r="6" stroke-opacity="0">
+                  <animate
+                    attributeName="r"
+                    begin="3s"
+                    dur="3s"
+                    values="6;22"
+                    calcMode="linear"
+                    repeatCount="indefinite"
+                  />
+                  <animate
+                    attributeName="stroke-opacity"
+                    begin="3s"
+                    dur="3s"
+                    values="1;0"
+                    calcMode="linear"
+                    repeatCount="indefinite"
+                  />
+                  <animate
+                    attributeName="stroke-width"
+                    begin="3s"
+                    dur="3s"
+                    values="2;0"
+                    calcMode="linear"
+                    repeatCount="indefinite"
+                  />
+                </circle>
+                <circle cx="22" cy="22" r="8">
+                  <animate
+                    attributeName="r"
+                    begin="0s"
+                    dur="1.5s"
+                    values="6;1;2;3;4;5;6"
+                    calcMode="linear"
+                    repeatCount="indefinite"
+                  />
+                </circle>
+              </g>
+            </svg>
+          )}
         </div>
 
         <input
@@ -92,7 +177,11 @@ const SearchBar = ({ scrollDirection }) => {
           id="search"
           name="search"
           placeholder="Search something.."
-          onChange={handleInputChange}
+          onChange={(e) => {
+            startTransition(() => {
+              setSearchtext(e.target.value);
+            });
+          }}
           onBlur={handleInputBlur}
           onFocus={handleInputFocus}
         />
